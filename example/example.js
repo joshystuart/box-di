df1292b9085cab029fedf893459d52a4147f26f1
Josh Stuart <josh@etaskr.com>
2016-09-07 15:41:28 +1000

    0.1.0 release.
    * Removed kontainer dependency.
    * No longer static so you can have multiple containers.
    * Added invokable.

--- example/example.js
+++ example/example.js
@@ -1,15 +1,25 @@
 import winston from 'winston';
 import TestFactory from './test-factory';
+import InvokableTest from './invokable-test';
 import Config from './config';
 import Box from '../lib/box';
 winston.level = 'debug';
 
 Box.setLogger(winston);
 
-Box.register('config', () => {
-    return Config;
-});
+Box.register('config', Config);
 Box.register('test', TestFactory);
+Box.registerInvokeable('invokableTest', InvokableTest, ['config']);
 
-const test = Box.get('test');
-console.log(test.getConfig());
+const test1 = Box.get('test');
+console.log(`test1: ${JSON.stringify(test1.getConfig())}`);
+
+const test2 = Box.get('test');
+console.log(`test2: ${JSON.stringify(test2.getConfig())}`);
+
+// the TestFactory uses Singleton=false
+console.log(`Is test1 and test2 the same: ${test1 === test2}`);
+
+const invokableTest = Box.get('invokableTest');
+console.log(`invokableTest.getData: ${invokableTest.getData()}`);
+console.log(`invokableTest.getConfig: ${JSON.stringify(invokableTest.getConfig())}`);
--- index.js
+++ index.js
@@ -1,13 +1,17 @@
 import Box from './lib/box';
 import inject from './lib/inject';
+import {scope, prototype, singleton} from './lib/scope';
 import FactoryInterface from './lib/factory-interface';
 
 // es5 export
 export default {
     Box: Box,
     inject: inject,
+    scope: scope,
+    prototype: prototype,
+    singleton: singleton,
     FactoryInterface: FactoryInterface
 };
 
 // es6 exports
-export {Box, inject, FactoryInterface};
+export {Box, inject, scope, singleton, prototype, FactoryInterface};
